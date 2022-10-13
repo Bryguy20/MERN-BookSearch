37eddb53a7e2ed28e2296157c072a3e9cf31e714
Bryguy <bryansegarra044@gmail.com>
2022-10-13 18:24:44 -0400

    added some changes

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// create new apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+// to intergrate our apollo server with express application as middleware
+server.applyMiddleware({app});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
